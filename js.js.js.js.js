c068989339a938da6a4d827382b9ae470b36500f
yeslol234 <72095827+yeslol234@users.noreply.github.com>
2020-10-22 16:10:53 -0700

    hmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm

---
+++ js.js.js.js.js
@@ -0,0 +1,11 @@
+//This creates a function called "identifyvariablex".
+function identifyvariablex(){
+  let x;
+  x=5
+}
+
+//This creates a function called "identifyvariabley".
+function identifyvariabley(){
+  let y;
+  y=10
+}
